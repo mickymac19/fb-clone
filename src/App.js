2bcec25c328fb3f06f23332ba635d956fca873e7
Micky Macato <57607248+mickymac19@users.noreply.github.com>
2021-05-30 23:33:49 +0900

    created components added styling and icons for the header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react"; 
 import './App.css';
+import Header from "./Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM naming convention
+    <div className="app">
+        <Header/>
+
+      {/* App body*/}
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */} 
+
+
+
     </div>
   );
 }
 
 export default App;
+  
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBPXwIqZChOzyEb-BK7QhpGPoHJgwp0sUI",
+    authDomain: "facebook-clone-19d61.firebaseapp.com",
+    projectId: "facebook-clone-19d61",
+    storageBucket: "facebook-clone-19d61.appspot.com",
+    messagingSenderId: "998327212762",
+    appId: "1:998327212762:web:4bad41dd835ef9541923e3",
+    measurementId: "G-91W2P55LBV"
+  };
