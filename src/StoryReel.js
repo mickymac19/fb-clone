915a7b3aedd22d3fc0d1d5e14c9f22a6e0e26f9a
Micky Macato <57607248+mickymac19@users.noreply.github.com>
2021-06-06 23:46:35 +0900

    finished StoryReel

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -5,10 +5,38 @@ import Story from "./Story";
 function StoryReel() {
     return (
         <div className="storyReel">
-            <Story />
-            <Story />
-            <Story />
-            <Story />
+
+            {/* <Story 
+                image="./micky.jpg"
+                title="Create Story"
+                /> */}
+            <Story 
+                image="./background6.jpg"
+                profileSrc="./yuri.jpg"
+                title="Nicky Niimura"
+            />
+              <Story 
+                image="./background4.jpg"
+                profileSrc="./jumar.png"
+                title="Jumar Macato"
+            />
+             <Story 
+                image="./background3.jpg"
+                profileSrc="./katrina.jpg"
+                title="Katrina Magbanua"
+            />
+             <Story 
+                image="./background1.jpg"
+                profileSrc="./april.jpg"
+                title="April Lizette Gonzales"
+            />
+             <Story 
+                image="./background2.jpg"
+                profileSrc="./erwine.jpg"
+                title="Erwine Ogawa"
+            />
+           
+           
         </div>
     )
 }
