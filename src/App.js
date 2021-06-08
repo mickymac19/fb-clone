ed9ad76f31a588f9157cc9455795d1e59033808c
Micky Macato <57607248+mickymac19@users.noreply.github.com>
2021-06-08 15:15:52 +0900

    added widgets

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed"; 
+import Widgets from "./Widgets"; 
 
 function App() {
+
+  const user = "micky"; 
   return (
     // BEM naming convention
     <div className="app">
-        <Header/>
-        <div className="app__body">
-          <Sidebar />
-          <Feed />
-          {/* <Widgets /> */}
-        </div>
+      {!user ? (<h1>Login</h1>) : 
+        (
+          <>
+            <Header/>
 
-
-   
-
-     
-
-      {/* Feed */}
-
-      {/* Widgets */} 
-
-
-
-    </div>
+            <div className="app__body">
+              <Sidebar />
+              <Feed />
+              <Widgets />  
+            </div>
+          </>
+        )}
+   </div>
   );
 }
 
